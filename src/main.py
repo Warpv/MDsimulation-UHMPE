1b9fbec9613c73ea39df49900a386eb59cd27842
NIKO-inc <118219943+NIKO-inc@users.noreply.github.com>
2024-02-28 19:32:16 +0300

    made base file for next sim sim_md_19kA and base .pdb file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 
+mol new /Users/niko/Desktop/VSCode/simulation-UHMW-PE-adhesion/xyz_coor_files/sim_md_11A.xyz autobonds no waitfor all
+
+
+/Users/niko/Desktop/VSCode/simulation-UHMW-PE-adhesion/xyz_coor_files/xyz_coor_files sim_md_11A.xyz
+
+set sel [atomselect top all]
+$sel set radius 0.95
+$sel set name A
+$sel set type A
+$sel set mass 1.0
+
+
+
